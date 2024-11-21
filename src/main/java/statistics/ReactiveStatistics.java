b4496f2d46faa18e74fdc992e89c7897ac60e066
Anna Ershova <a.i.ershova@vk.teams>
2024-11-21 19:29:36 +0300

    Убран CountDownLatch

--- src/main/java/statistics/ReactiveStatistics.java
+++ src/main/java/statistics/ReactiveStatistics.java
@@ -29,14 +29,15 @@ public class ReactiveStatistics {
         }, BackpressureStrategy.BUFFER);
 
         Map<Manufacturer, StatisticsAccumulator> statistics = new ConcurrentHashMap<>();
-        CountDownLatch latch = new CountDownLatch(1);
+        //CountDownLatch latch = new CountDownLatch(1);
 
         productFlowable
                 .subscribeOn(Schedulers.io())
                 .observeOn(Schedulers.computation())
-                .subscribe(new RegulatedStatisticsSubscriber(statistics, batchSize, latch));
+                .subscribe(new RegulatedStatisticsSubscriber(statistics, batchSize));
+                //.subscribe(new RegulatedStatisticsSubscriber(statistics, batchSize, latch));
 
-        latch.await();
+        //latch.await();
         return finalizeStatistics(statistics);
     }
 
@@ -51,14 +52,14 @@ public class ReactiveStatistics {
     static class RegulatedStatisticsSubscriber implements Subscriber<Product> {
         private final Map<Manufacturer, StatisticsAccumulator> statistics;
         private final int batchSize;
-        private final CountDownLatch latch;
+        //private final CountDownLatch latch;
         private Subscription subscription;
         private int processed = 0;
 
-        public RegulatedStatisticsSubscriber(Map<Manufacturer, StatisticsAccumulator> statistics, int batchSize, CountDownLatch latch) {
+        public RegulatedStatisticsSubscriber(Map<Manufacturer, StatisticsAccumulator> statistics, int batchSize) {
             this.statistics = statistics;
             this.batchSize = batchSize;
-            this.latch = latch;
+            //this.latch = latch;
         }
 
         @Override
@@ -82,12 +83,12 @@ public class ReactiveStatistics {
         @Override
         public void onError(Throwable t) {
             t.printStackTrace();
-            latch.countDown();
+            //latch.countDown();
         }
 
         @Override
         public void onComplete() {
-            latch.countDown();
+            //latch.countDown();
         }
     }
 
